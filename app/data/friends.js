1a3edd23626fefad86eb7d086671a767d7c62834
Michael Gentilini <michael@dfwqqualityappraisal.com>
2019-06-01 20:29:07 -0500

    got home and survey with buttons resolving on local host

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+// ! FriendFinder starter (DATA)
+var friends = [{
+    "name": "Yoda",
+    "photo": "https://vignette.wikia.nocookie.net/starwars/images/d/d6/Yoda_SWSB.png/",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    "name": "Jon Snow",
+    "photo": "https://www.cheatsheet.com/wp-content/uploads/2019/05/Jon-Snow-episode-6-640x448.jpg",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    "name": "Tyrion Lannister",
+    "photo": "https://pmctvline2.files.wordpress.com/2019/05/game-of-thrones-season-8-tyrion-lannister-peter-dinklage.jpg",
+    "scores": [5, 4, 5, 5, 5, 3, 4, 4, 1, 4]
+  }
+];
